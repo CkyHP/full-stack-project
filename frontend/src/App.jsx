38a74ce5604ed4556eb10bbb4538499a5a6f4a2f
CkyHP <160540639+CkyHP@users.noreply.github.com>
2025-02-24 22:09:30 -0300

    Estrutura básica do projeto
    A estrutura básica do projeto. Fiz basicamente para aprender um pouco mais sobre React e Flask, e devo expandir futuramente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,60 @@
 import { useState, useEffect } from 'react'
 import ContactList from './ContactList'
 import './App.css'
+import ContactForm from './ContactForm'
 
 function App() {
-    const [contacts, setContacts] = useState([])
+    const [contacts, setContacts] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentContact, setCurrentContact] = useState({})
 
     useEffect(() => {
       fetchContacts()
     }, [])
 
     const fetchContacts = async () => {
-      const response = await fetch('http://127.0.0.1:5000/contacts')
+      const response = await fetch('http://127.0.0.1:5000/contacts');
       const data = await response.json()
-      setContacts(data)
-      console.log(data.contacts)
+      setContacts(data.contacts)
+    }
+
+    const closeModal = () => {
+      setIsModalOpen(false)
+      setCurrentContact({})
+    }
+
+    const openCreateModal = () => {
+      if (!isModalOpen) {
+        setIsModalOpen(true)
+      }
+    }
+
+    const openEditModal = (contact) => {
+      if (isModalOpen) return
+      setCurrentContact(contact)
+      setIsModalOpen(true)
+    }
+
+    const onUpdate = () => {
+      closeModal()
+      fetchContacts()
     }
      
-  return <ContactList contacts={contacts} />
+  return(
+  <>
+      <div className='contact-list'>
+      <ContactList contacts={contacts} updateContact={openEditModal} updateCallBack={onUpdate}/>
+      <button onClick={openCreateModal}>Create New Contact</button>
+      </div>
+      { isModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ContactForm existingContact={currentContact} updateCallBack={onUpdate} />
+        </div>
+      </div>}
+  
+  </> 
+  )
 }
 
 export default App
